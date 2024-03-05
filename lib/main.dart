3118ee8eb11e2f3111490405115f86d4a7190dd8
roshanyacham <roshanyacham301@gmail.com>
2024-03-05 11:31:07 +0530

    changed color

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         title: 'Namer App',
         theme: ThemeData(
           useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+          colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 162, 80, 58)),
         ),
         home: MyHomePage(),
       ),
