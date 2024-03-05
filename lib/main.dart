f94a31d742bed49745fdf2c1e2586e1eeb3707f8
roshanyacham <roshanyacham301@gmail.com>
2024-03-05 11:34:30 +0530

    changed name home to home1

--- lib/main.dart
+++ lib/main.dart
@@ -75,7 +75,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 destinations: [
                   NavigationRailDestination(
                     icon: Icon(Icons.home),
-                    label: Text('Home'),
+                    label: Text('Home1'),
                   ),
                   NavigationRailDestination(
                     icon: Icon(Icons.favorite),
